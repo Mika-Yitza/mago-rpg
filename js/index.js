fc1334db464aeabafa97425e22c0932eb5e1aa68
Mihai Sarbu <mihaisarbu@Mihais-MacBook-Pro.local>
2021-01-20 07:12:24 +0000

    add lesson 1

---
+++ js/index.js
@@ -0,0 +1,43 @@
+const title = 'MAGO rpg'
+const heroClassStats = [
+    {
+        name : 'Tester',
+        health : 10,
+        coding : 1,
+        hacking : 3
+    },
+    {
+        name : 'Developer',
+        health : 8,
+        coding : 3,
+        hacking : 2
+    }
+]
+
+window.onload = function() {
+    const classes = ['Tester', 'Developer']
+
+    for(let className of classes){
+        document.getElementById(`class`).append(new Option(className, className))
+    }
+    document.getElementById(`title`).innerHTML = title
+}
+
+document.getElementById(`create`).onclick = function() {
+    const charName = document.getElementById(`name`).value
+    const charClass = document.getElementById(`class`).value
+
+    for(let stats of heroClassStats){
+        if(stats.name == charClass){
+            document.getElementById(`panel`).innerHTML = charName + '\n\n Class: ' + charClass +
+            '\n HP: ' + stats.health + '\n Coding: ' + stats.coding + '\n Hacking: ' + stats.hacking
+        }
+    }
+}
+
+document.getElementById(`fight`).onclick = function() {
+    const charName = document.getElementById(`name`).value
+    const oppName = document.getElementById(`opponent`).value
+
+    document.getElementById(`combat`).innerHTML = charName + ' is ready to fight ' + oppName
+}
